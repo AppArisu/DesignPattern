886948673d46d607f27c269bc0144e5a2def49a4
AppArisu <fate-ryu.2001@docomo.ne.jp>
2022-11-13 02:26:50 +0900

    Chain_of_Responsibility作成

--- DesignPattern/Sauce/Chain_of_Responsibility.h
+++ DesignPattern/Sauce/Chain_of_Responsibility.h
@@ -1,10 +1,111 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <string>
 /*
     Chain of Responsibility （責任の連鎖） は、 振る舞いに関するデザインパターンの一つで、 ハンドラーの連鎖に沿ってリクエストを渡すことができます。
     各ハンドラーは、 リクエストを受け取ると、 リクエストを処理するか、 連鎖内の次のハンドラーに渡すかを決めます。
 */
 
-class Chain_of_Responsibility
+// Handlerインターフェースは、ハンドラーのチェーンを構築するためのメソッドを宣言します。
+// また、要求を実行するためのメソッドも宣言します。
+class Handler
 {
-
+public:
+    virtual Handler* SetNext(Handler* handler) = 0;
+    virtual std::string Handle(std::string requires) = 0;
 };
+
+// デフォルトの連鎖動作は、ベース・ハンドラー・クラスの中に実装することができます。
+class AbstractHandler :public Handler
+{
+private:
+    Handler* next_handler_;
+
+public:
+    AbstractHandler() :next_handler_(nullptr) {}
+
+    Handler* SetNext(Handler* handler) override 
+    {
+        // ここからハンドラを返すと、次のような便利な方法でハンドラをリンクできます。
+        this->next_handler_ = handler;
+        return handler;
+    }
+
+    std::string Handle(std::string requires) override
+    {
+        if (this->next_handler_)
+        {
+            return this->next_handler_->Handle(requires);
+        }
+        return {};
+    }
+};
+
+// すべてのConcrete Handlerは、要求を処理するか、チェーン内の次のハンドラーに要求を渡します。
+class MonkeyHandler : public AbstractHandler {
+ public:
+  std::string Handle(std::string request) override {
+    if (request == "バナナ") {
+        return "サル:" + request + "を食べます.\n";
+    } else {
+      return AbstractHandler::Handle(request);
+    }
+  }
+};
+class SquirrelHandler : public AbstractHandler {
+ public:
+  std::string Handle(std::string request) override {
+    if (request == "ナッツ") {
+        return "リス:" + request + "を食べます.\n";
+    } else {
+      return AbstractHandler::Handle(request);
+    }
+  }
+};
+class DogHandler : public AbstractHandler {
+ public:
+  std::string Handle(std::string request) override {
+    if (request == "ミートボール") {
+        return "イヌ:" + request + "を食べます.\n";
+    } else {
+      return AbstractHandler::Handle(request);
+    }
+  }
+};
+
+// クライアント・コードは通常、単一のハンドラを操作するのに適しています。
+// ほとんどの場合、クライアント・コードはハンドラがチェーンの一部であることさえ認識しません。
+void ClientCode(Handler& handler)
+{
+    std::vector<std::string> food = { "ナッツ", "バナナ", "コーヒー一杯" };
+    for (const std::string& f : food) {
+        std::cout << "クライアント: " << f << "が欲しい人？\n";
+        const std::string result = handler.Handle(f);
+        if (!result.empty()) {
+            std::cout << "  " << result;
+        }
+        else {
+            std::cout << "  " << f << " 手つかずのままだった.\n";
+        }
+    }
+}
+
+void CofR_Start()
+{
+    MonkeyHandler* monkey = new MonkeyHandler;
+    SquirrelHandler* squirrel = new SquirrelHandler;
+    DogHandler* dog = new DogHandler;
+    monkey->SetNext(squirrel)->SetNext(dog);
+    //squirrel->SetNext(monkey)->SetNext(dog);
+
+    std::cout << "クライアント: サル > リス > イヌ\n\n";
+    ClientCode(*monkey);
+    std::cout << "\n";
+    std::cout << "サブチェイン: リス > イヌ\n\n";
+    ClientCode(*squirrel);
+
+    delete monkey;
+    delete squirrel;
+    delete dog;
+}
--- DesignPattern/Sauce/Main.cpp
+++ DesignPattern/Sauce/Main.cpp
@@ -7,7 +7,74 @@
 // 構築
 #include"Structure.h"
 
+enum Behavior
+{
+    Chain_of_Responsibility = 1,
+    Command,
+    Iterator,
+    Mediator,
+    Memento,
+    Observer,
+    State,
+    Strtegy,
+    Template_Method,
+    Visitor
+};
+
+enum Generation
+{
+    Abstract_Factory = 1,
+    Builder,
+    Factory_Method,
+    Prototype,
+    Singleton
+};
+
+enum Structure
+{
+    Adapter = 1,
+    Bridge,
+    Composite,
+    Decorator,
+    Facade,
+    Flyweight,
+    Proxy
+};
+
 int main()
 {
+    int stateChanger = 0;
 
+    std::cout << "振る舞いに関するのデザインパターンの例を選ぶ：";
+    std::cin >> stateChanger;
+
+    switch (stateChanger)
+    {
+    case Behavior::Chain_of_Responsibility:
+        CofR_Start();
+        break;
+    case Behavior::Command:
+        break;
+    case Behavior::Iterator:
+        break;
+    case Behavior::Mediator:
+        break;
+    case Behavior::Memento:
+        break;
+    case Behavior::Observer:
+        break;
+    case Behavior::State:
+        break;
+    case Behavior::Strtegy:
+        break;
+    case Behavior::Template_Method:
+        break;
+    case Behavior::Visitor:
+        break;
+    default:
+        std::cout << "何もせず終了します" << std::endl;
+        break;
+    };
+
+    return 0;
 }
